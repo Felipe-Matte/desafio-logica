ab56e248e70392558bcb83e23b618bf3edb91003
felipe-Matte <felipematte4@yahoo.com>
2024-03-19 20:50:05 -0300

    ajustes

--- main1.js
+++ main1.js
@@ -12,7 +12,7 @@ if  (xp <= 1000){
 	nivel = "Prata";
 }	else if (xp >= 5001 && xp <= 7000){
 	nivel = "Ouro";
-}	else if (xp >= 7001 && xp <= 8001){
+}	else if (xp >= 7001 && xp <= 8000){
 	nivel = "Platina";
 }	else if (xp >= 8001 && xp <= 9000){
 	nivel = "Ascendente";
